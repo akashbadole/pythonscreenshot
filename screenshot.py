0b8c14598b5af0e58ccedf504f8e779eae4f5ec3
unknown <akashbadole@rediffmail.com>
2020-12-11 22:19:23 +0530

    GUI created

--- screenshot.py
+++ screenshot.py
@@ -1,13 +1,35 @@
 import time
 import pyautogui
+import tkinter as tk 
 
 def screenshot():
     name = int(round(time.time() * 1000))
     name = 'C:/Users/Akash/Documents/Python-learning/screenshot/screenshot_data/{}.png'.format(name)
-    time.sleep(5)
+    # time.sleep(5) GUI Wait till 5sec
+    time.sleep(2)
     img  = pyautogui.screenshot(name)
     img.show()
 
-screenshot()
+# screenshot()
 
 # during run pillo required message come, cmd administator mode pip install Pillow --upgrade and finally run
+
+root  = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text = "Take Screenshot",
+    command = screenshot
+)
+button.pack(side = tk.LEFT)
+
+close = tk.Button(
+    frame,
+    text = "QUIT",
+    command = quit
+)
+close.pack(side = tk.LEFT)
+
+root.mainloop()
